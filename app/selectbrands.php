73da89c7a15737d595abe0c95363f8e05e5eb99b
Sakaauti <sami.autio@outlook.com>
2016-03-07 18:49:26 +0200

    test

--- app/selectbrands.php
+++ app/selectbrands.php
@@ -1,19 +1,17 @@
 <?php
-require_once('/db-init.php');
+require_once('../db-init.php');
 
 $sql = <<<SQLEND
 SELECT brand FROM brand
 SQLEND;
 
-
 $stmt = $db->prepare("$sql");
 $stmt->execute();
 
 if ($stmt->rowCount() >= 1) {
-    echo "asd";
+    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+        echo $row['brand'] . ' <br /> ';
+    }   
 }
-
-echo "yasd";
-
 
 ?>
