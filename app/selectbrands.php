0ab98ea872d32ff9088b3d410e097688dd220419
Sakaauti <H8543@student.jamk.fi>
2016-03-07 14:00:42 +0200

    dbtest

---
+++ app/selectbrands.php
@@ -0,0 +1,19 @@
+<?php
+require_once('/db-init.php');
+
+$sql = <<<SQLEND
+SELECT brand FROM brand
+SQLEND;
+
+
+$stmt = $db->prepare($sql);
+$stmt->execute();
+
+if ($stmt->rowCount() >= 1) {
+    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+        echo "$row[brand]";
+    }
+}
+
+
+?>
---
+++ db-init.php
@@ -0,0 +1,13 @@
+<?php
+try {
+    // db-init.php
+    $db = new PDO('mysql:host=178.62.246.71;dbname=drugbank_01;charset=utf8',
+                  'teamj', 'ZFntr83!(55%3f');
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+}
+catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+    die();
+}
+?>
