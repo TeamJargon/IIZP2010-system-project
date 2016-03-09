2948788954988f01fa3a6eb1be8f7d104f0d899e
Sakaauti <sami.autio@outlook.com>
2016-03-09 22:54:05 +0200

    Added: getform, getsubstance

---
+++ app/getform.php
@@ -0,0 +1,25 @@
+<?php
+require_once('../db-init.php');
+
+$brand = $_GET['brand'];
+
+$sql = <<<SQLEND
+SELECT Form FROM form WHERE PrimaryKey = ANY (SELECT Form FROM medicine WHERE Brand = (SELECT PrimaryKey FROM brand WHERE Brand = :brand));
+SQLEND;
+
+$query = $db->prepare("$sql");
+$query->bindValue(':brand', $brand, PDO::PARAM_STR);
+$query->execute();
+
+if ($query->rowCount() >= 1) {
+    $rows = array();
+    while ($row = $query->fetch(PDO::FETCH_ASSOC)) {
+        $rows[] = $row['Form'];
+    }
+    
+    echo json_encode($rows);
+} else {
+    echo null;
+}
+
+?>
