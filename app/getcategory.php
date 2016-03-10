7176f01276be07a938f3520db9b62654bd1866ce
Sakaauti <sami.autio@outlook.com>
2016-03-10 08:46:48 +0200

    Added: countbrands, countsubstances, getcategory

---
+++ app/getcategory.php
@@ -0,0 +1,25 @@
+<?php
+require_once('../db-init.php');
+
+$brand = $_GET['brand'];
+
+$sql = <<<SQLEND
+SELECT Classification FROM classification WHERE PrimaryKey = ANY (SELECT Classification FROM medicine WHERE Brand = (SELECT PrimaryKey FROM brand WHERE brand = :brand));
+SQLEND;
+
+$query = $db->prepare("$sql");
+$query->bindValue(':brand', $brand, PDO::PARAM_STR);
+$query->execute();
+
+if ($query->rowCount() >= 1) {
+    $rows = array();
+    while ($row = $query->fetch(PDO::FETCH_ASSOC)) {
+        $rows[] = $row['Classification'];
+    }
+    
+    echo json_encode($rows);
+} else {
+    echo null;
+}
+
+?>
