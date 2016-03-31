812113364b750bc83dfd4dae0af14659c11cffc6
H8244 <h8244@student.jamk.fi>
2016-03-31 11:47:58 +0300

    Fix bankview

--- app/js/lists.js
+++ app/js/lists.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
         dataType: "json",
         success: function(response) {
             for (var i = 0; i < response.length; i++) {
-                $(".brands").append("<li><input class=btn type=button value='"+ response[i] +"' onclick=selectBrand('"+ encodeURIComponent(response[i]) +"') </li>");
+                $("#CraftingBrands").append("<li><input class=btn type=button value='"+ response[i] +"' onclick=selectBrand('"+ encodeURIComponent(response[i]) +"') </li>");
             }
         }
     });
@@ -17,7 +17,7 @@ $(document).ready(function() {
         dataType: "json",
         success: function(response) {
             for (var i = 0; i < response.length; i++) {
-                $(".substances").append("<li><input class=btn type=button value='"+ response[i] +"' onclick=selectSubstance('"+ encodeURIComponent(response[i]) +"') </li>");
+                $("#CraftingSubstances").append("<li><input class=btn type=button value='"+ response[i] +"' onclick=selectSubstance('"+ encodeURIComponent(response[i]) +"') </li>");
             }
         }
     });
@@ -28,7 +28,7 @@ $(document).ready(function() {
         dataType: "json",
         success: function(response) {
             for (var i = 0; i < response.length; i++) {
-                $(".forms").append("<li><input class=btn type=button value='"+ response[i] +"' onclick=selectForm('"+ encodeURIComponent(response[i]) +"') </li>");
+                $("#CraftingForms").append("<li><input class=btn type=button value='"+ response[i] +"' onclick=selectForm('"+ encodeURIComponent(response[i]) +"') </li>");
             }
         }
     });
@@ -43,12 +43,33 @@ $(document).ready(function() {
             }
         }
     });    
+	
+	/* -------------------------------------------------------------------- */
+	/* -------------------------------------------------------------------- */
+	
+	    $.ajax({  
+        type: 'POST',  
+        url: 'php/selectbrands.php', 
+        data: { },
+        dataType: "json",
+        success: function(response) {
+            for (var i = 0; i < response.length; i++) {
+                $("#BankBrands").append("<li><input class=btn type=button value='"+ response[i] +"' onclick=selectBankMedicine('"+ encodeURIComponent(response[i]) +"') </li>");
+            }
+        }
+    });  
 });
 
 var selectedBrand = "";
 var selectedSubstances = [];
 var selectedForm = "";
 var selectedCategory = "";
+
+function selectBankMedicine(brand)
+{
+	selectedBrand = decodeURIComponent(brand);
+	
+}
 
 function selectBrand(brand) {
     selectedBrand = decodeURIComponent(brand);
