a1d3cafecc762b4a3e9ad98196f8a42817713735
vuoale <H8346@student.jamk.fi>
2016-03-31 13:31:00 +0300

    problemSolved flag

--- app/problem.js
+++ app/problem.js
@@ -2,8 +2,9 @@ function generateProblem() {
 	$("#patient").show("slide", { direction: "right" }, 1000, function(){
 		$("#problem").fadeIn("slow"); 
 		if(problemSolved == true)
+		{
 			problemSolved = false;
 			GenerateChallenge();
-		
+		}	
 	});
 }
